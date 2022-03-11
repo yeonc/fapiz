af0a5f7ddab0db054df72db8cb1e6267f3cf4bd3
yeonc <chsynee@gmail.com>
2022-03-12 01:13:07 +0900

    폴더 구조 변경, pages 폴더 안에 있는 파일들의 확장자를 .js에서 .jsx로 변경

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import ProTip from '../src/ProTip'
-import Link from '../src/Link'
-import Copyright from '../src/Copyright'
+import ProTip from '../ProTip'
+import Link from '../Link'
+import Copyright from '../Copyright'
 
 export default function Index() {
   return (
