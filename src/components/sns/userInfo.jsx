23887d98f21b6e5522ca5c99dcf90067485ca4a8
yeonc <chsynee@gmail.com>
2022-04-02 16:50:21 +0900

    로그인 한 유저 본인이 아닌 다른 유저의 SNS 페이지에 있을 경우에만 팔로잉, 메시지 버튼이 보이도록 수정

--- src/components/sns/userInfo.jsx
+++ src/components/sns/userInfo.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react'
 import UserProfile from 'components/sns/userProfile'
 import UserButtons from 'components/sns/userButtons'
+import useGetRequest from 'hooks/useGetRequest'
+import { BACKEND_URL, IS_SERVER } from 'constants/constants'
 
 const UserInfo = ({ user }) => {
+  const [snsPageUserId, setSnsPageUserId] = useState(null)
+  const [loggedInUserId, setLoggedInUserId] = useState(null)
+  const isMe = snsPageUserId === loggedInUserId
+
+  const token = !IS_SERVER && localStorage.getItem('jwt')
+  const { response } = useGetRequest(BACKEND_URL, '/api/users/me', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  useEffect(() => {
+    if (response !== null) {
+      const loggedInUserInfo = response.data
+      setLoggedInUserId(loggedInUserInfo.id)
+      setSnsPageUserId(user?.id)
+    }
+  }, [response, user?.id])
+
   return (
     <header>
       <UserProfile user={user} />
-      <UserButtons />
+      {isMe || <UserButtons />}
     </header>
   )
 }
