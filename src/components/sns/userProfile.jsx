d2931c1b039b0df5e01e1373c61110221a9ed161
yeonc <chsynee@gmail.com>
2022-06-07 14:16:40 +0900

    SNS 페이지에서 팔로워/팔로잉 한 유저 목록을 보여주는 모달창 구현 (유저 목록 데이터는 넣기 전)

--- src/components/sns/userProfile.jsx
+++ src/components/sns/userProfile.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
 import styled from '@emotion/styled'
 import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import FollowerListModal from 'components/utils/followerListModal'
+import FollowingListModal from 'components/utils/followingListModal'
 import visuallyHidden from 'styles/visuallyHidden'
 import { horizontal, mgRight } from 'styles/layout'
 import { BACKEND_URL } from 'constants/constants'
@@ -16,31 +20,56 @@ const UserAvatar = ({ profileImageUrl, username }) => (
   />
 )
 
-const UserProfileText = ({ username, weight, height, follower, following }) => (
-  <div>
-    <h1>{username}</h1>
-    <dl css={horizontal}>
-      <div css={mgRight(8)}>
-        <dt css={visuallyHidden}>키</dt>
-        <dd>{height}cm</dd>
-      </div>
-      <div>
-        <dt css={visuallyHidden}>몸무게</dt>
-        <dd>{weight}kg</dd>
-      </div>
-    </dl>
-    <dl css={horizontal}>
-      <div css={[mgRight(18), horizontal]}>
-        <dt css={mgRight(4)}>팔로워</dt>
-        <dd>{follower?.length}</dd>
-      </div>
-      <div css={horizontal}>
-        <dt css={mgRight(4)}>팔로잉</dt>
-        <dd>{following?.length}</dd>
-      </div>
-    </dl>
-  </div>
-)
+const UserProfileText = ({ username, weight, height, follower, following }) => {
+  const [followingModalOpen, setFollowingModalOpen] = useState(false)
+  const [followerModalOpen, setFollowerModalOpen] = useState(false)
+  const handleFollowingModalOpen = () => setFollowingModalOpen(true)
+  const handleFollowingModalClose = () => setFollowingModalOpen(false)
+  const handleFollowerModalOpen = () => setFollowerModalOpen(true)
+  const handleFollowerModalClose = () => setFollowerModalOpen(false)
+
+  return (
+    <div>
+      <h1>{username}</h1>
+      <dl css={horizontal}>
+        <div css={mgRight(8)}>
+          <dt css={visuallyHidden}>키</dt>
+          <dd>{height}cm</dd>
+        </div>
+        <div>
+          <dt css={visuallyHidden}>몸무게</dt>
+          <dd>{weight}kg</dd>
+        </div>
+      </dl>
+      <dl css={horizontal}>
+        <div css={[mgRight(18), horizontal]}>
+          <dt css={mgRight(4)}>팔로워</dt>
+          <dd>
+            <Button variant="text" onClick={handleFollowerModalOpen}>
+              {follower?.length}
+            </Button>
+            <FollowerListModal
+              onClose={handleFollowerModalClose}
+              open={followerModalOpen}
+            />
+          </dd>
+        </div>
+        <div css={horizontal}>
+          <dt css={mgRight(4)}>팔로잉</dt>
+          <dd>
+            <Button variant="text" onClick={handleFollowingModalOpen}>
+              {following?.length}
+            </Button>
+            <FollowingListModal
+              onClose={handleFollowingModalClose}
+              open={followingModalOpen}
+            />
+          </dd>
+        </div>
+      </dl>
+    </div>
+  )
+}
 
 const UserProfile = ({ user }) => (
   <UserProfileWrapper>
---
+++ src/components/utils/followerListModal.jsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+
+const modalBoxStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
+const FollowerListModal = ({ onClose, open }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalBoxStyle}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Follower
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
+  )
+}
+
+export default FollowerListModal
