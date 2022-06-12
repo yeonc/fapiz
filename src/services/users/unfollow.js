3f94b4e36004dc506209fbda74b3dee991638a4c
yeonc <chsynee@gmail.com>
2022-06-12 16:02:19 +0900

    unfollow 함수 수정
    수정 사항: 마지막 axios 호출 리턴, 공통 url 변수 선언, 에러 처리

--- src/services/users/unfollow.js
+++ src/services/users/unfollow.js
@@ -1,22 +1,36 @@
 import axios from 'axios'
 import { BACKEND_URL } from 'constants/constants'
 
+// TODO: unfollow를 구현할 수 있는 더 좋은 방법 고민해 보기
 const unfollow = async ({ myId, targetUserId, myFollowingUserIds }) => {
-  return axios({
+  const url = `${BACKEND_URL}/api/users/${myId}`
+
+  axios({
     method: 'put',
-    url: `${BACKEND_URL}/api/users/${myId}`,
+    url,
     data: {
       following: null,
     },
-  }).then(() => {
-    axios({
-      method: 'put',
-      url: `${BACKEND_URL}/api/users/${myId}`,
-      data: {
-        following: myFollowingUserIds.filter(id => id !== targetUserId),
-      },
-    })
   })
+    // 첫 번째 put 요청에서 에러 발생 시 following 데이터 복구
+    .catch(() => {
+      axios({
+        method: 'put',
+        url,
+        data: {
+          following: myFollowingUserIds,
+        },
+      })
+    })
+    .then(() => {
+      return axios({
+        method: 'put',
+        url,
+        data: {
+          following: myFollowingUserIds.filter(id => id !== targetUserId),
+        },
+      })
+    })
 }
 
 export default unfollow
