5bd19850c8d2904cceb70592ea77e7442df59421
yeonc <chsynee@gmail.com>
2022-06-13 20:34:07 +0900

    createComment 함수 만들고 사용

--- src/components/sns/postCommentContents.jsx
+++ src/components/sns/postCommentContents.jsx
@@ -6,6 +6,10 @@ import EditIcon from '@mui/icons-material/Edit'
 import DeleteIcon from '@mui/icons-material/Delete'
 import UserAvatar from 'components/common/images/userAvatar'
 import { horizontal } from 'styles/layout'
+import useMe from 'hooks/useMe'
+import createComment from 'services/users/createComment'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const CommentInputFormWrapper = styled.form`
   display: flex;
@@ -30,19 +34,55 @@ const comments = [
   },
 ]
 
-const PostCommentInputForm = () => (
-  <CommentInputFormWrapper>
-    <UserAvatar
-      profileImageUrl="/"
-      username="john"
-      styleConfig={{ width: 30, height: 30, marginRight: 1 }}
-    />
-    <TextField label="댓글을 입력하세요" variant="standard" />
-    <Button variant="contained" size="small">
-      등록
-    </Button>
-  </CommentInputFormWrapper>
-)
+const PostCommentInputForm = ({ snsPostId }) => {
+  const [comment, setComment] = useState('')
+  const { me, error } = useMe()
+
+  const loading = !me && !error
+
+  if (loading) {
+    return <p>로딩중...</p>
+  }
+
+  if (error) {
+    return <p>에러가 발생했습니다. 홈으로 돌아가세요</p>
+  }
+
+  const handleChange = e => {
+    setComment(e.target.value)
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      await createComment({ comment, postId: snsPostId, authorId: me.id })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setComment('')
+    }
+  }
+
+  return (
+    <CommentInputFormWrapper onSubmit={handleSubmit}>
+      <UserAvatar
+        profileImageUrl={me.profileImage.url}
+        username={me.username}
+        styleConfig={{ width: 30, height: 30, marginRight: 1 }}
+      />
+      <TextField
+        label="댓글을 입력하세요"
+        variant="standard"
+        value={comment}
+        onChange={handleChange}
+        required
+      />
+      <Button variant="contained" size="small" type="submit">
+        등록
+      </Button>
+    </CommentInputFormWrapper>
+  )
+}
 
 const PostCommentList = () => (
   <ul>
@@ -61,11 +101,16 @@ const PostCommentList = () => (
   </ul>
 )
 
-const PostCommentContents = () => (
-  <>
-    <PostCommentInputForm />
-    <PostCommentList />
-  </>
-)
+const PostCommentContents = () => {
+  const router = useRouter()
+  const { snsPostId } = router.query
+
+  return (
+    <>
+      <PostCommentInputForm snsPostId={snsPostId} />
+      <PostCommentList />
+    </>
+  )
+}
 
 export default PostCommentContents
