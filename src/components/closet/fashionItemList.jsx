86430f08bdc67edcc62499c1962f8827a63eeeb1
yeonc <chsynee@gmail.com>
2022-07-04 23:05:17 +0900

    상대경로로 되어 있던 import 경로를 절대경로로 변경

--- src/components/closet/fashionItemList.jsx
+++ src/components/closet/fashionItemList.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import FashionItemListItem from './fashionItemListItem'
+import FashionItemListItem from 'components/closet/fashionItemListItem'
 
 const StyledFashionItemListWrapper = styled.div`
   display: flex;
