3e886d339aca4212d2e158fe9253380fe964924b
yeonc <chsynee@gmail.com>
2022-07-05 11:23:40 +0900

    포스트 수정 관련 로직을 PostEditForm 컴포넌트 내부로 옮기고 컴포넌트 이름도 변경 (→ PostEdit)

---
+++ src/components/sns/edit/postEdit.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react'
+import editSnsPost from 'services/users/editSnsPost'
+import uploadImage from 'services/users/uploadImage'
+import generateIdToObject from 'utils/generateIdToObject'
+import { changeImageFilesToPreviewImages } from 'utils/previewImage'
+import { BACKEND_URL } from 'constants/constants'
+
+const EMPTY_FASHION_ITEM_INFO = { category: '', price: '', buyingPlace: '' }
+
+const PostEdit = ({ snsPost, afterEditPost, children }) => {
+  const initialPreviewImages = snsPost.attributes.postImages.data.map(
+    image => ({
+      url: BACKEND_URL + image.attributes.url,
+      altText: image.attributes.alternativeText,
+    })
+  )
+  const newEmptyFashionItemInfo = generateIdToObject(EMPTY_FASHION_ITEM_INFO)
+  const initialFashionItemsInfo = snsPost.attributes.fashionIemsInfo ?? [
+    newEmptyFashionItemInfo,
+  ]
+  const initialPostText = snsPost.attributes.content
+
+  const [imageFiles, setImageFiles] = useState(null)
+  const [previewImages, setPreviewImages] = useState(initialPreviewImages)
+  const [fashionItemsInfo, setFashionItemsInfo] = useState(
+    initialFashionItemsInfo
+  )
+  const [postText, setPostText] = useState(initialPostText)
+
+  const handleImageFilesChange = imageFiles => {
+    setImageFiles(imageFiles)
+    const previewImagesFromImageFiles =
+      changeImageFilesToPreviewImages(imageFiles)
+    setPreviewImages(previewImagesFromImageFiles)
+  }
+
+  const handleFashionItemsInfoChange = fashionItemsInfo => {
+    setFashionItemsInfo(fashionItemsInfo)
+  }
+
+  const handleFashionItemInfoAddMoreButtonClick = () => {
+    setFashionItemsInfo(prev => {
+      const newEmptyFashionItemInfo = generateIdToObject(
+        EMPTY_FASHION_ITEM_INFO
+      )
+      return prev.concat(newEmptyFashionItemInfo)
+    })
+  }
+
+  const handleFashionItemInfoDeleteButtonClick = fashionItemInfoIdToDelete => {
+    setFashionItemsInfo(prev => {
+      return prev.filter(prev => prev.id !== fashionItemInfoIdToDelete)
+    })
+  }
+
+  const handlePostTextChange = postText => {
+    setPostText(postText)
+  }
+
+  const editPostWithImage = async uploadedImageIds => {
+    return editSnsPost({
+      postId: snsPost.id,
+      content: postText,
+      imageIds: uploadedImageIds,
+      fashionItemsInfo,
+    })
+  }
+
+  const editPostWithoutImage = async () => {
+    return editSnsPost({
+      postId: snsPost.id,
+      content: postText,
+      fashionItemsInfo,
+    })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      if (!imageFiles) {
+        await editPostWithoutImage()
+      }
+
+      if (imageFiles) {
+        const res = await uploadImage(imageFiles)
+        const uploadedImageIds = res.data.map(image => image.id)
+        await editPostWithImage(uploadedImageIds)
+      }
+
+      afterEditPost()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return children({
+    imageFiles,
+    previewImages,
+    fashionItemsInfo,
+    postText,
+    handleImageFilesChange,
+    handleFashionItemsInfoChange,
+    handleFashionItemInfoAddMoreButtonClick,
+    handleFashionItemInfoDeleteButtonClick,
+    handlePostTextChange,
+    handleSubmit,
+  })
+}
+
+export default PostEdit
--- src/pages/sns/post/edit/[snsPostId].jsx
+++ src/pages/sns/post/edit/[snsPostId].jsx
@@ -1,117 +1,73 @@
-import { useState } from 'react'
 import { useRouter } from 'next/router'
 import withHeader from 'hocs/withHeader'
 import Button from '@mui/material/Button'
-import PostEditForm from 'components/sns/edit/postEditForm'
+import PostEdit from 'components/sns/edit/postEdit'
 import ImageUploadButton from 'components/common/buttons/imageUploadButton'
 import PostImages from 'components/sns/edit/postImages'
 import FashionItemsInfo from 'components/sns/edit/fashionItemsInfo'
 import PostText from 'components/sns/edit/postText'
 import useSnsPost from 'hooks/useSnsPost'
-import { BACKEND_URL } from 'constants/constants'
-import generateIdToObject from 'utils/generateIdToObject'
-import { changeImageFilesToPreviewImages } from 'utils/previewImage'
-
-const EMPTY_FASHION_ITEM_INFO = { category: '', price: '', buyingPlace: '' }
 
 const SnsPostEditPage = () => {
   const router = useRouter()
   const { snsPostId } = router.query
 
-  const { snsPost, isLoading: isSnsPostLoading } = useSnsPost(snsPostId)
-
-  const initialPreviewImages = snsPost
-    ? snsPost.attributes.postImages.data.map(image => ({
-        url: BACKEND_URL + image.attributes.url,
-        altText: image.attributes.alternativeText,
-      }))
-    : []
-  const newEmptyFashionItemInfo = generateIdToObject(EMPTY_FASHION_ITEM_INFO)
-  const initialFashionItemsInfo =
-    snsPost && snsPost.attributes.fashionItemsInfo
-      ? snsPost.attributes.fashionIemsInfo
-      : [newEmptyFashionItemInfo]
-  const initialPostText = snsPost ? snsPost.attributes.content : ''
-
-  const [imageFiles, setImageFiles] = useState(null)
-  const [previewImages, setPreviewImages] = useState(initialPreviewImages)
-  const [fashionItemsInfo, setFashionItemsInfo] = useState(
-    initialFashionItemsInfo
-  )
-  const [postText, setPostText] = useState(initialPostText)
-
-  const handleImageFilesChange = imageFiles => {
-    setImageFiles(imageFiles)
-    const previewImagesFromImageFiles =
-      changeImageFilesToPreviewImages(imageFiles)
-    setPreviewImages(previewImagesFromImageFiles)
-  }
-
-  const handleFashionItemsInfoChange = fashionItemsInfo => {
-    setFashionItemsInfo(fashionItemsInfo)
-  }
-
-  const handleFashionItemInfoAddMoreButtonClick = () => {
-    setFashionItemsInfo(prev => {
-      const newEmptyFashionItemInfo = generateIdToObject(
-        EMPTY_FASHION_ITEM_INFO
-      )
-      return prev.concat(newEmptyFashionItemInfo)
-    })
-  }
-
-  const handleFashionItemInfoDeleteButtonClick = fashionItemInfoIdToDelete => {
-    setFashionItemsInfo(prev => {
-      return prev.filter(prev => prev.id !== fashionItemInfoIdToDelete)
-    })
-  }
-
-  const handlePostTextChange = postText => {
-    setPostText(postText)
-  }
+  const { snsPost } = useSnsPost(snsPostId)
 
   const afterEditPost = () => {
     router.push(`/sns/post/${snsPostId}`)
   }
 
-  if (isSnsPostLoading) {
-    return <p>SNS post 정보를 불러오는 중입니다.</p>
+  if (!snsPost) {
+    return null
   }
 
   return (
-    <PostEditForm
-      snsPostId={snsPostId}
-      imageFiles={imageFiles}
-      fashionItemsInfo={fashionItemsInfo}
-      postText={postText}
-      afterEditPost={afterEditPost}
-    >
-      <PostImages
-        imageFiles={imageFiles}
-        previewImages={previewImages}
-        onImageFilesChange={handleImageFilesChange}
-        imageUploadButton={
-          <ImageUploadButton
+    <PostEdit snsPost={snsPost} afterEditPost={afterEditPost}>
+      {({
+        imageFiles,
+        previewImages,
+        fashionItemsInfo,
+        postText,
+        handleImageFilesChange,
+        handleFashionItemsInfoChange,
+        handleFashionItemInfoAddMoreButtonClick,
+        handleFashionItemInfoDeleteButtonClick,
+        handlePostTextChange,
+        handleSubmit,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <PostImages
+            imageFiles={imageFiles}
+            previewImages={previewImages}
             onImageFilesChange={handleImageFilesChange}
-            buttonAriaLabel="SNS 게시물 이미지 수정"
+            imageUploadButton={
+              <ImageUploadButton
+                onImageFilesChange={handleImageFilesChange}
+                buttonAriaLabel="SNS 게시물 이미지 수정"
+              />
+            }
           />
-        }
-      />
-      <FashionItemsInfo
-        fashionItemsInfo={fashionItemsInfo}
-        onFashionItemsInfoChange={handleFashionItemsInfoChange}
-        onFashionItemInfoAddMoreButtonClick={
-          handleFashionItemInfoAddMoreButtonClick
-        }
-        onFashionItemInfoDeleteButtonClick={
-          handleFashionItemInfoDeleteButtonClick
-        }
-      />
-      <PostText postText={postText} onPostTextChange={handlePostTextChange} />
-      <Button variant="contained" type="submit">
-        수정
-      </Button>
-    </PostEditForm>
+          <FashionItemsInfo
+            fashionItemsInfo={fashionItemsInfo}
+            onFashionItemsInfoChange={handleFashionItemsInfoChange}
+            onFashionItemInfoAddMoreButtonClick={
+              handleFashionItemInfoAddMoreButtonClick
+            }
+            onFashionItemInfoDeleteButtonClick={
+              handleFashionItemInfoDeleteButtonClick
+            }
+          />
+          <PostText
+            postText={postText}
+            onPostTextChange={handlePostTextChange}
+          />
+          <Button variant="contained" type="submit">
+            수정
+          </Button>
+        </form>
+      )}
+    </PostEdit>
   )
 }
 
