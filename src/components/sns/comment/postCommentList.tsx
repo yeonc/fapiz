c7397afa0570620c4d81f3150768a0c129eb75a6
yeonc <chsynee@gmail.com>
2022-07-17 18:45:01 +0900

    PostCommentList 컴포넌트 수정 (가독성 개선, 탈퇴 유저 케이스 제거)

--- src/components/sns/comment/postCommentList.tsx
+++ src/components/sns/comment/postCommentList.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import Avatar from '@mui/material/Avatar'
 import Link from '@mui/material/Link'
 import CommentByMode from 'components/sns/comment/commentByMode'
@@ -6,6 +7,12 @@ import useSnsComments from 'hooks/useSnsComments'
 import createUrlQuery from 'utils/createUrlQuery'
 import { BACKEND_URL } from 'constants/constants'
 import ROUTE_URL from 'constants/routeUrl'
+
+const StyledCommentAuthorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
+`
 
 const PostCommentList = ({ snsPostId }) => {
   const query = createUrlQuery({
@@ -16,48 +23,46 @@ const PostCommentList = ({ snsPostId }) => {
   })
   const { snsComments: snsCommentsFromStrapi } = useSnsComments(query)
 
-  const comments = snsCommentsFromStrapi.map((snsComment: any) => ({
-    id: snsComment.id,
-    content: snsComment.attributes.content,
-    author: snsComment.attributes.author.data.attributes.username,
-    isAuthorNotExist: snsComment.attributes.author.data.attributes.isHidden,
-    authorId: snsComment.attributes.author.data.id,
-    authorProfileImageUrl: snsComment.attributes.author.data.attributes
-      .profileImage.data
-      ? BACKEND_URL +
-        snsComment.attributes.author.data.attributes.profileImage.data
-          .attributes.url
-      : undefined,
-  }))
+  const comments = snsCommentsFromStrapi.map((snsComment: any) => {
+    const author = snsComment.attributes.author.data
+
+    return {
+      id: snsComment.id,
+      content: snsComment.attributes.content,
+      authorId: author.id,
+      authorName: author.attributes.username,
+      authorProfileImageUrl: author.attributes.profileImage.data
+        ? BACKEND_URL + author.attributes.profileImage.data.attributes.url
+        : undefined,
+    }
+  })
+
+  if (comments.length === 0) {
+    return <p>댓글이 없습니다. 첫 댓글을 입력해 보세요!</p>
+  }
 
   return (
     <ul>
-      {comments.length === 0 ? (
-        <p>댓글이 없습니다. 첫 댓글을 입력해 보세요!</p>
-      ) : (
-        comments.map((comment: any) => (
-          <li key={comment.id} css={horizontal}>
-            <Link href={`${ROUTE_URL.SNS}/${comment.authorId}`}>
+      {comments.map((comment: any) => (
+        <li key={comment.id} css={horizontal}>
+          <Link href={`${ROUTE_URL.SNS}/${comment.authorId}`}>
+            <StyledCommentAuthorWrapper>
               <Avatar
                 alt={comment.authorProfileImageUrl}
                 src={comment.authorProfileImageUrl}
                 sx={{ width: 30, height: 30, marginRight: 1 }}
               />
-            </Link>
-            <Link href={`${ROUTE_URL.SNS}/${comment.authorId}`}>
-              <span>
-                {comment.isAuthorNotExist ? '탈퇴한 유저' : comment.author}
-              </span>
-            </Link>
-            <CommentByMode
-              commentId={comment.id}
-              commentText={comment.content}
-              snsPostId={snsPostId}
-              authorId={comment.authorId}
-            />
-          </li>
-        ))
-      )}
+              <span>{comment.authorName}</span>
+            </StyledCommentAuthorWrapper>
+          </Link>
+          <CommentByMode
+            commentId={comment.id}
+            commentText={comment.content}
+            snsPostId={snsPostId}
+            authorId={comment.authorId}
+          />
+        </li>
+      ))}
     </ul>
   )
 }
