4857efad287a61b18595c5cf6943b0692c7dd007
yeonc <chsynee@gmail.com>
2022-07-18 19:09:07 +0900

    SnsPage 컴포넌트 수정 (탈퇴한 유저의 SNS 페이지로 가면 이전 페이지로 리다이렉트 시켰던 코드 삭제)

--- src/pages/sns/[userId].tsx
+++ src/pages/sns/[userId].tsx
@@ -5,7 +5,6 @@ import Fab from '@mui/material/Fab'
 import CreateIcon from '@mui/icons-material/Create'
 import UserInfo from 'components/sns/userInfo'
 import SnsPosts from 'components/sns/snsPosts'
-import PreviousPageRedirect from 'components/common/redirect/previousPageRedirect'
 import useUser from 'hooks/useUser'
 import useMe from 'hooks/useMe'
 import addBackendUrlToImageUrl from 'utils/addBackendUrlToImageUrl'
@@ -15,9 +14,6 @@ const positionOfSnsPostCreateButton = css`
   bottom: 30px;
   right: 30px;
 `
-
-const REDIRECT_TO_PREVIOUS_PAGE_ALERT_MESSAGE =
-  '존재하지 않는 유저입니다. 이전 페이지로 이동합니다'
 
 const SnsPage = () => {
   const router = useRouter()
@@ -49,25 +45,17 @@ const SnsPage = () => {
 
   return (
     <>
-      {user.isHidden ? (
-        <PreviousPageRedirect
-          alertMessage={REDIRECT_TO_PREVIOUS_PAGE_ALERT_MESSAGE}
-        />
-      ) : (
-        <>
-          <UserInfo user={user} />
-          <SnsPosts userId={user.id} />
-          {isMySnsPage && (
-            <Fab
-              color="primary"
-              aria-label="SNS 게시물 등록"
-              css={positionOfSnsPostCreateButton}
-              onClick={handleSnsPostCreateButtonClick}
-            >
-              <CreateIcon />
-            </Fab>
-          )}
-        </>
+      <UserInfo user={user} />
+      <SnsPosts userId={user.id} />
+      {isMySnsPage && (
+        <Fab
+          color="primary"
+          aria-label="SNS 게시물 등록"
+          css={positionOfSnsPostCreateButton}
+          onClick={handleSnsPostCreateButtonClick}
+        >
+          <CreateIcon />
+        </Fab>
       )}
     </>
   )
