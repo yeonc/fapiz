b6e71f661b8d960849c47fa56b1e27f384d54010
yeonc <chsynee@gmail.com>
2022-07-18 17:36:18 +0900

    UserInfo 컴포넌트 수정 (불필요한 flex 선언 제거, 조건부 렌더링을 위해 선언한 변수의 가독성 개선)

--- src/components/sns/userInfo.tsx
+++ src/components/sns/userInfo.tsx
@@ -15,30 +15,13 @@ const StyledUserInfoWrapper = styled.header`
   align-items: center;
 `
 
-const StyledUserInfoRightWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const StyledUserProfileTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const StyledFollowWrapper = styled.div`
-  display: flex;
-`
-
-const StyledButtonsWrapper = styled.div`
-  display: flex;
-`
-
 const UserInfo = ({ user }) => {
   const { mutate } = useSWRConfig()
 
   const { me } = useMe()
 
   const isMySnsPage = user.id === me?.id
-  const isButtonsShow = me && !isMySnsPage
+  const isLoggedIn = !!me
 
   const refetch = () =>
     mutate(user.id ? { url: `/api/users/${user.id}` } : null)
@@ -54,44 +37,38 @@ const UserInfo = ({ user }) => {
         src={user.profileImageUrl}
         sx={{ width: 130, height: 130, marginRight: 4 }}
       />
-      <StyledUserInfoRightWrapper>
-        <StyledUserProfileTextWrapper>
-          <Typography variant="h4" component="h1">
-            {user.username}
-          </Typography>
-          <dl css={horizontal}>
-            {user.height && (
-              <div css={mgRight(8)}>
-                <dt css={visuallyHidden}>키</dt>
-                <dd>{user.height}cm</dd>
-              </div>
-            )}
-            {user.weight && (
-              <div>
-                <dt css={visuallyHidden}>몸무게</dt>
-                <dd>{user.weight}kg</dd>
-              </div>
-            )}
-          </dl>
-        </StyledUserProfileTextWrapper>
-        <StyledFollowWrapper>
-          <Follower followers={user.followers} afterFollow={afterFollow} />
-          <Following followings={user.followings} afterFollow={afterFollow} />
-        </StyledFollowWrapper>
-        <StyledButtonsWrapper>
-          {isButtonsShow && (
-            <>
-              <FollowToggleButton
-                myId={me.id}
-                myFollowings={me.followings}
-                targetUserId={user.id}
-                afterFollow={afterFollow}
-              />
-              <MessageButton userId={user.id} />
-            </>
+      <div>
+        <Typography variant="h4" component="h1">
+          {user.username}
+        </Typography>
+        <dl css={horizontal}>
+          {user.height && (
+            <div css={mgRight(8)}>
+              <dt css={visuallyHidden}>키</dt>
+              <dd>{user.height}cm</dd>
+            </div>
           )}
-        </StyledButtonsWrapper>
-      </StyledUserInfoRightWrapper>
+          {user.weight && (
+            <div>
+              <dt css={visuallyHidden}>몸무게</dt>
+              <dd>{user.weight}kg</dd>
+            </div>
+          )}
+        </dl>
+        <Follower followers={user.followers} afterFollow={afterFollow} />
+        <Following followings={user.followings} afterFollow={afterFollow} />
+        {isLoggedIn && !isMySnsPage && (
+          <div>
+            <FollowToggleButton
+              myId={me.id}
+              myFollowings={me.followings}
+              targetUserId={user.id}
+              afterFollow={afterFollow}
+            />
+            <MessageButton userId={user.id} />
+          </div>
+        )}
+      </div>
     </StyledUserInfoWrapper>
   )
 }
