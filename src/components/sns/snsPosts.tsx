8686faacfc2ca84069174bcdb523ee13c74f0234
yeonc <chsynee@gmail.com>
2022-07-18 14:18:09 +0900

    SnsPosts 컴포넌트 리팩토링
    snsPosts 데이터 정제 다시 함 (꼭 필요한 것들만 들어가 있도록)
    컴포넌트 조건부 렌더링 코드 가독성 개선
    img 태그에서 사용하지 않는 속성들 제거

--- src/components/sns/snsPosts.tsx
+++ src/components/sns/snsPosts.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
+import { css } from '@emotion/react'
 import SnsPostList from '@mui/material/ImageList'
 import SnsPostItem from '@mui/material/ImageListItem'
-import { css } from '@emotion/react'
 import useSnsPosts from 'hooks/useSnsPosts'
 import createUrlQuery from 'utils/createUrlQuery'
 import addBackendUrlToImageUrl from 'utils/addBackendUrlToImageUrl'
@@ -18,51 +18,41 @@ const SnsPosts = ({ userId }) => {
     'filters[author][id][$eq]': userId,
   })
 
-  const { snsPosts, isLoading } = useSnsPosts(query)
+  const { snsPosts: snsPostsFromStrapi, isLoading } = useSnsPosts(query)
 
   if (isLoading) {
     return <p>게시물을 불러오는 중입니다...</p>
   }
 
-  const sanitizedSnsPosts = snsPosts.map((post: any) => {
-    const postId = post.id
-    const postImageArray = post.attributes.postImages.data
-    const postImages = postImageArray
-      ? postImageArray.map((postImage: any) => postImage.attributes)
-      : []
+  const snsPosts = snsPostsFromStrapi.map((post: any) => ({
+    id: post.id,
+    firstImage: {
+      url: addBackendUrlToImageUrl(
+        post.attributes.postImages.data[0].attributes.url
+      ),
+      altText: post.attributes.postImages.data[0].attributes.alternativeText,
+    },
+  }))
 
-    return {
-      postId,
-      postImages,
-    }
-  })
+  const goToSnsPost = postId => {
+    router.push(`/sns/post/${postId}`)
+  }
 
-  return sanitizedSnsPosts.length === 0 ? (
-    <p>작성된 포스트가 없습니다.</p>
-  ) : (
+  if (snsPosts.length === 0) {
+    return <p>작성된 포스트가 없습니다.</p>
+  }
+
+  return (
     <SnsPostList sx={{ width: 650 }} cols={3}>
-      {sanitizedSnsPosts.map((post: any) => {
-        const firstImage = post.postImages[0]
-        const firstImageUrl = addBackendUrlToImageUrl(firstImage.url)
-        const goToSnsPost = () => {
-          router.push(`/sns/post/${post.postId}`)
-        }
-
-        return (
-          <SnsPostItem
-            key={firstImageUrl}
-            onClick={goToSnsPost}
-            css={cursorPointer}
-          >
-            <img
-              src={`${firstImageUrl}?w=164&h=164&fit=crop&auto=format`}
-              srcSet={`${firstImageUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              alt={firstImage && firstImage.alternativeText}
-              loading="lazy"
-            />
-          </SnsPostItem>
-        )
-      })}
+      {snsPosts.map((snsPost: any) => (
+        <SnsPostItem
+          key={snsPost.firstImage.url}
+          onClick={() => goToSnsPost(snsPost.id)}
+          css={cursorPointer}
+        >
+          <img src={snsPost.firstImage.url} alt={snsPost.firstImage.altText} />
+        </SnsPostItem>
+      ))}
     </SnsPostList>
   )
 }
